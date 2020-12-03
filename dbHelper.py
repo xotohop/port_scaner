7e0d563b783992b8ccf356a076726b89e525f01d
kr33t9k <rustamov.rakhman2001@gmail.com>
2020-12-03 19:38:10 +0300

    Исправлена ошибка сравнения

--- dbHelper.py
+++ dbHelper.py
@@ -33,12 +33,15 @@ def insertData(c, conn, listOfData: list):
 
 def compare(c, conn, host: str, listOfData: list):
     chages = []
-    c.execute(f"""SELECT * FROM scans WHERE date = (SELECT MAX(date) FROM scans GROUP BY host HAVING date = '{host}') AND host = '{host}';""")
+    c.execute(f"""SELECT host, port FROM scans WHERE date = (SELECT MAX(date) FROM scans WHERE host = '{host}') AND host = '{host}';""")
     previousData = c.fetchall()
     for item in previousData:
-        if (list(item) in listOfData):
-            listOfData.remove(list(item))
-        else:
+        flag = False
+        for i in listOfData:
+            if (i[1:] == list(item)):
+                flag = True
+                listOfData.remove(i)
+        if (flag == False):
             chages.append(list(item))
     if (chages == [] and listOfData == []):
         return f'\nИзменений с последнего сканирования не обнаружено.'
@@ -50,30 +53,32 @@ def compare(c, conn, host: str, listOfData: list):
         return f'\nСписок портов которые закрылись после прошлого сканирования:\n{chages}.'
 
 
-# # Пример использования
-createDB(c, conn)
+# # # Пример использования
+# createDB(c, conn)
 
-now = str(datetime.now())
+# now = str(datetime.now())
 
-insertData(c, conn, [[now, '1', -1], [now, '45.33.32.1576', 1], [now, '45.33.32.156', 2], [now, '216.58.210.174', 3], [now, '45.33.32.156', 4]]) ###Не забывать добавлять значение -1 после каждого сканирования
+# insertData(c, conn, [[now, '1', -1], [now, '45.33.32.1576', 1], [now, '45.33.32.156', 2], [now, '216.58.210.174', 3], [now, '45.33.32.156', 4]]) ###Не забывать добавлять значение -1 после каждого сканирования
 
-data = getData(c, conn, ports=[1, 2, 3, 5], hosts=['45.33.32.156', '216.58.210.174', '45.33.32.1576'], startDate="2020-12-03 17:45:46.797540", endDate="2020-12-03 17:45:46.797540") #Время не получается сравнить ((((
+# data = getData(c, conn, ports=[1, 2, 3, 5], hosts=['45.33.32.156', '216.58.210.174', '45.33.32.1576'], startDate="2020-12-03 17:45:46.797540", endDate="2020-12-03 17:45:46.797540") #Время не получается сравнить ((((
 
-for item in data:
-    print(item)
+# for item in data:
+#     print(item)
 
 
-data2 = compare(c, conn, host='45.33.32.156', listOfData=[[now, '45.33.32.156', 2]])
+# data2 = compare(c, conn, host='45.33.32.156', listOfData=[[now, '45.33.32.156', 2], [now, '45.33.32.156', 13]])
 
-print(data2)
+# print(data2)
 
 ## result
 #
 ##   ***Открытые порты***
 #  
-##   ('2020-12-03 18:42:33.282198', '45.33.32.1576', 1)
-##   ('2020-12-03 18:42:33.282198', '45.33.32.156', 2)
-##   ('2020-12-03 18:42:33.282198', '216.58.210.174', 3)
+##   ('2020-12-03 19:36:31.884272', '45.33.32.1576', 1)
+##   ('2020-12-03 19:36:31.884272', '45.33.32.156', 2)
+##   ('2020-12-03 19:36:31.884272', '216.58.210.174', 3)
 #  
-##   Список портов, которые были закрыты при прошлом сканировании:
-##   [['2020-12-03 18:42:33.282198', '45.33.32.156', 2]].
+##   Список портов, которые открылись после прошлого сканирования:
+##   [['2020-12-03 19:36:31.884272', '45.33.32.156', 13]].
+##   Список портов которые закрылись после прошлого сканирования:
+##   [['45.33.32.156', 4]].
